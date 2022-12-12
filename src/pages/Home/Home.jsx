dc3aa50c99df5aeacf154dcf4e38a822d0ca04f1
rodrigo <rodrigolibragawebdev@gmail.com>
2022-12-12 19:42:43 -0300

    First Commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -38,7 +38,7 @@ const Home = () => {
         {loading && <p> Carregando...</p>}
         {/*  o erro estava em lançar {h3...} e não sem chaves */}
         {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
-        {posts && posts.length === 0 && (
+        {!posts && (
           <div className={styles.noposts}>
             <p>Não foram encontrados posts</p>
             <Link className="btn" to="/posts/create">
